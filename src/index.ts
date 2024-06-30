e1c608f14545534af2a76ddaa331fd8f38e6622c
kamatil-dev <karim.amahtil@gmail.com>
2024-07-01 00:55:05 +0100

    feat: use function instead of class, prevent converting leading 0 strings to numbers

--- src/index.ts
+++ src/index.ts
@@ -1,147 +1,146 @@
+type valueT = string | number | boolean | null | undefined;
+type objT = Record<string, valueT>;
+type arrT = valueT[];
+
+const specialCharsArr = [",", ":", "[", "]", "{", "}"];
+const specialChars = new Set(specialCharsArr);
+let index: number;
+let s: string;
+
+function stringify(obj: valueT | objT | arrT): string {
+	if (obj === undefined || obj === null) return String(obj);
+
+	if (Array.isArray(obj)) return `[${obj.map(stringify).join(",")}]`;
+
+	if (typeof obj === "object" && obj !== null) {
+		const objKeys = Object.keys(obj);
+		const result: string[] = [];
+
+		for (const key of objKeys) result.push(`${key}:${stringify(obj[key])}`);
+
+		return `{${result.join(",")}}`;
+	}
+	const stringifiedObj = String(obj);
+	return specialCharsArr.some((char) => stringifiedObj.indexOf(char))
+		? escapeSpecialChars(stringifiedObj)
+		: stringifiedObj;
+}
+
+function unstringify(input: string) {
+	if (input === "null") return null;
+	if (input === "undefined") return undefined;
+
+	index = 0;
+	s = input;
+
+	return parseValue();
+}
+
+function parseArray() {
+	const array: arrT = [];
+	const len = s.length;
+
+	index++; // skip '['
+	while (index < len && s[index] !== "]") {
+		array.push(parseValue() as valueT);
+		if (s[index] === ",") index++; // skip ','
+	}
+	index++; // skip ']'
+	return array;
+}
+
+function parseObject() {
+	const obj: objT = {};
+	const len = s.length;
+
+	index++; // skip '{'
+	while (index < len && s[index] !== "}") {
+		const [key, value] = parseKeyValue();
+		obj[key] = value;
+
+		if (s[index] === ",") index++; // skip ','
+	}
+	index++; // skip '}'
+	return obj;
+}
+
+function parseKeyValue(): [string, valueT] {
+	const key = parseValue() as string;
+
+	if (s[index] !== ":") throw new Error('Expected ":" after key');
+
+	index++; // skip ':'
+	const value = parseValue() as valueT;
+
+	return [key, value];
+}
+
+function parseValue(): valueT | objT | arrT {
+	const len = s.length;
+	const start = index;
+
+	if (s[index] === "[" || s[index] === "{")
+		return s[index] === "[" ? parseArray() : parseObject();
+
+	while (index < len && !specialChars.has(s[index]))
+		index += s[index] === "\\" && specialChars.has(s[index + 1]) ? 2 : 1; // Skip both '\\' and ','
+
+	const parsedValue = s.slice(start, index);
+
+	if (parsedValue === "") return null;
+	if (parsedValue.indexOf("\\") !== -1)
+		return unescapeSpecialChars(parsedValue);
+	if (parsedValue === "false") return false;
+	if (parsedValue === "null") return null;
+	if (parsedValue === "undefined") return undefined;
+	if (parsedValue === "true") return true;
+
+	const numParsedValue = Number(parsedValue);
+
+	// Check if the parsed value is a number and convert it
+	if (
+		Number.isInteger(numParsedValue) &&
+		parsedValue !== null &&
+		parsedValue.at(0) !== "0"
+	)
+		return numParsedValue;
+
+	return parsedValue;
+}
+
+function unescapeSpecialChars(value: string): string {
+	let unescapedValue = "";
+	const len = value.length;
+
+	for (let i = 0; i < len; i++) {
+		if (value[i] === "\\" && i + 1 < len) {
+			const nextChar = value[i + 1];
+			if (specialChars.has(nextChar)) {
+				unescapedValue += nextChar;
+				i++;
+				continue;
+			}
+		}
+		unescapedValue += value[i];
+	}
+
+	return unescapedValue;
+}
+
+function escapeSpecialChars(value: string): string {
+	let escapedValue = "";
+	const len = value.length;
+
+	for (let i = 0; i < len; i++) {
+		const char = value[i];
+		if (specialChars.has(char)) escapedValue += "\\";
+		escapedValue += char;
+	}
+
+	return escapedValue;
+}
+
 export default class Inison {
-  private static specialCharsArr = [",", ":", "[", "]", "{", "}"];
-  private static specialChars = new Set(Inison.specialCharsArr);
-  private static index: number;
-  private static s: string;
-
-  static stringify(obj: any): string {
-    if (obj === undefined || obj === null) return String(obj);
-
-    if (Array.isArray(obj))
-      return "[" + obj.map(Inison.stringify).join(",") + "]";
-
-    if (typeof obj === "object" && obj !== null) {
-      const objKeys = Object.keys(obj);
-      let result: string[] = [];
-
-      for (const key of objKeys)
-        result.push(`${key}:${Inison.stringify(obj[key])}`);
-
-      return `{${result.join(",")}}`;
-    }
-    obj = String(obj);
-    return Inison.specialCharsArr.some((char) => obj.indexOf(char))
-      ? Inison.escapeSpecialChars(obj)
-      : obj;
-  }
-
-  static unstringify(s: string): any {
-    if (s === "null") return null;
-    if (s === "undefined") return undefined;
-
-    Inison.index = 0;
-    Inison.s = s;
-
-    return Inison.parseValue();
-  }
-
-  private static parseArray(): any[] {
-    const array: any[] = [];
-    const len = Inison.s.length;
-
-    Inison.index++; // skip '['
-    while (Inison.index < len && Inison.s[Inison.index] !== "]") {
-      array.push(Inison.parseValue());
-      if (Inison.s[Inison.index] === ",") Inison.index++; // skip ','
-    }
-    Inison.index++; // skip ']'
-    return array;
-  }
-
-  private static parseObject(): any {
-    const obj: any = {};
-    const len = Inison.s.length;
-
-    Inison.index++; // skip '{'
-    while (Inison.index < len && Inison.s[Inison.index] !== "}") {
-      const [key, value] = Inison.parseKeyValue();
-      obj[key] = value;
-
-      if (Inison.s[Inison.index] === ",") Inison.index++; // skip ','
-    }
-    Inison.index++; // skip '}'
-    return obj;
-  }
-
-  private static parseKeyValue(): [any, any] {
-    const key = Inison.parseValue();
-
-    if (Inison.s[Inison.index] !== ":")
-      throw new Error('Expected ":" after key');
-
-    Inison.index++; // skip ':'
-    const value = Inison.parseValue();
-
-    return [key, value];
-  }
-
-  private static parseValue(): any {
-    const len = Inison.s.length;
-    const start = Inison.index;
-
-    if (Inison.s[Inison.index] === "[" || Inison.s[Inison.index] === "{")
-      return Inison.s[Inison.index] === "["
-        ? Inison.parseArray()
-        : Inison.parseObject();
-
-    while (
-      Inison.index < len &&
-      !Inison.specialChars.has(Inison.s[Inison.index])
-    )
-      Inison.index +=
-        Inison.s[Inison.index] === "\\" &&
-        Inison.specialChars.has(Inison.s[Inison.index + 1])
-          ? 2
-          : 1; // Skip both '\\' and ','
-
-    const parsedValue = Inison.s.slice(start, Inison.index);
-
-    if (parsedValue === "") return null;
-    else if (parsedValue.indexOf("\\") !== -1)
-      return Inison.unescapeSpecialChars(parsedValue);
-    else if (parsedValue === "false") return false;
-    else if (parsedValue === "null") return null;
-    else if (parsedValue === "undefined") return undefined;
-    else if (parsedValue === "true") return true;
-
-    const numParsedValue = Number(parsedValue);
-
-    // Check if the parsed value is a number and convert it
-    if (Number.isInteger(numParsedValue) && parsedValue !== null)
-      return numParsedValue;
-
-    return parsedValue;
-  }
-
-  private static unescapeSpecialChars(value: string): string {
-    let unescapedValue = "";
-    const len = value.length;
-
-    for (let i = 0; i < len; i++) {
-      if (value[i] === "\\" && i + 1 < len) {
-        const nextChar = value[i + 1];
-        if (Inison.specialChars.has(nextChar)) {
-          unescapedValue += nextChar;
-          i++;
-          continue;
-        }
-      }
-      unescapedValue += value[i];
-    }
-
-    return unescapedValue;
-  }
-
-  private static escapeSpecialChars(value: string): string {
-    let escapedValue = "",
-      len = value.length;
-
-    for (let i = 0; i < len; i++) {
-      const char = value[i];
-      if (Inison.specialChars.has(char)) escapedValue += "\\";
-      escapedValue += char;
-    }
-
-    return escapedValue;
-  }
+	static stringify = stringify;
+	static unstringify = unstringify;
 }
